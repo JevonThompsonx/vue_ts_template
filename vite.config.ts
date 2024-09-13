2aa55992bfcf40d9e983d0c889e6add08643eb25
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-09-12 19:26:02 -0700

    Update vite.config.ts
    Put wrong port before

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import VueRouter from 'unplugin-vue-router/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3000, // Replace with your desired port number
+    port: 8080, // Replace with your desired port number
   },
   plugins: [vue(),tailwindcss(),VueRouter()],
 })
